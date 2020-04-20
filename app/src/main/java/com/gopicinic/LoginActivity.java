fc8fe2b358abf6ccaa72ad2c1c2ae05a98bc03b3
srinath <srinathballa96>
2020-04-20 12:59:01 -0400

    added intent in login activty

--- app/src/main/java/com/gopicinic/LoginActivity.java
+++ app/src/main/java/com/gopicinic/LoginActivity.java
@@ -1,14 +1,38 @@
 package com.gopicinic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button LogInButton, RegisterButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LogInButton = (Button) findViewById(R.id.buttonLogin);
+        RegisterButton = (Button) findViewById(R.id.buttonRegister);
+        LogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
