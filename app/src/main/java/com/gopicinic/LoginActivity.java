654ce405e87babe2ff88c676e73049ac88c50264
srinath <srinathballa96>
2020-04-22 14:52:22 -0400

    modified login/signup screen

--- app/src/main/java/com/gopicinic/LoginActivity.java
+++ app/src/main/java/com/gopicinic/LoginActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,19 +18,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
     EditText Email, Password;
     Button LogInButton, RegisterButton;
     FirebaseAuth mAuth;
-    FirebaseAuth.AuthStateListener mAuthListner;
-    FirebaseUser mUser;
     String email, password;
     ProgressDialog dialog;
-    public static final String userEmail = "";
-
     public static final String TAG = "LOGIN";
 
     @Override
@@ -47,27 +41,10 @@ public class LoginActivity extends AppCompatActivity {
         Password = (EditText) findViewById(R.id.editPassword);
         dialog = new ProgressDialog(this);
         mAuth = FirebaseAuth.getInstance();
-        mUser = FirebaseAuth.getInstance().getCurrentUser();
-        mAuthListner = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (mUser != null) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                } else {
-                    Log.d(TAG, "AuthStateChanged:Logout");
-                }
-
-            }
-        };
         LogInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 userSign();
-
-
             }
         });
 
@@ -80,23 +57,17 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mAuth.removeAuthStateListener(mAuthListner);
 
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mAuthListner != null) {
-            mAuth.removeAuthStateListener(mAuthListner);
-        }
-
     }
 
     @Override
@@ -128,34 +99,14 @@ public class LoginActivity extends AppCompatActivity {
 
                 } else {
                     dialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login successfull", Toast.LENGTH_SHORT).show();
 
-                    checkIfEmailVerified();
-
+                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
-
     }
-
-    private void checkIfEmailVerified() {
-        FirebaseUser users = FirebaseAuth.getInstance().getCurrentUser();
-        boolean emailVerified = users.isEmailVerified();
-        if (!emailVerified) {
-            Toast.makeText(this, "Verify the Email Id", Toast.LENGTH_SHORT).show();
-            mAuth.signOut();
-            finish();
-        } else {
-            Email.getText().clear();
-
-            Password.getText().clear();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-
-            intent.putExtra(userEmail, email);
-
-            startActivity(intent);
-
-        }
-    }
-
 }
 
