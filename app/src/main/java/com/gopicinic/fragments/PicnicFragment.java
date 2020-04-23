d140c58ddfdbc9c55882eceb92901d02699a555a
srinath <srinathballa96>
2020-04-22 21:29:57 -0400

    added category picnic

---
+++ app/src/main/java/com/gopicinic/fragments/PicnicFragment.java
@@ -0,0 +1,21 @@
+package com.gopicinic.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.gopicinic.R;
+
+
+public class PicnicFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_picnic, container, false);
+        return root;
+    }
+}
