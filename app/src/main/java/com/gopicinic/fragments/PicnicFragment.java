39a67f9f842acf300832f3130aeb89b5b570c1e2
srinath <srinathballa96>
2020-04-23 12:24:22 -0400

    Final code

--- app/src/main/java/com/gopicinic/fragments/PicnicFragment.java
+++ app/src/main/java/com/gopicinic/fragments/PicnicFragment.java
@@ -1,21 +1,194 @@
 package com.gopicinic.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.gopicinic.R;
+import com.gopicinic.database.DatabaseHelper;
+import com.gopicinic.database.NotesAdapter;
+import com.gopicinic.database.model.Note;
+import com.gopicinic.utils.MyDividerItemDecoration;
+import com.gopicinic.utils.RecyclerTouchListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PicnicFragment extends Fragment {
+    private NotesAdapter mAdapter;
+    private List<Note> notesList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TextView noNotesView;
+
+    private DatabaseHelper db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_picnic, container, false);
+
+        recyclerView = root.findViewById(R.id.recycler_view);
+        noNotesView = root.findViewById(R.id.empty_notes_view);
+
+        db = new DatabaseHelper(getContext());
+
+        notesList.addAll(db.getAllNotes(Note.PICNIC_TABLE_NAME));
+
+        FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNoteDialog(false, null, -1);
+            }
+        });
+
+        mAdapter = new NotesAdapter(getContext(), notesList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(mAdapter);
+
+        toggleEmptyNotes();
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(),
+                recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, final int position) {
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                showActionsDialog(position);
+            }
+        }));
         return root;
+    }
+
+    private void createNote(String note) {
+        long id = db.insertNote(note,Note.PICNIC_TABLE_NAME);
+
+        Note n = db.getNote(id, Note.PICNIC_TABLE_NAME);
+
+        if (n != null) {
+            notesList.add(0, n);
+
+            mAdapter.notifyDataSetChanged();
+
+            toggleEmptyNotes();
+        }
+    }
+
+    private void updateNote(String note, int position) {
+        Note n = notesList.get(position);
+        n.setNote(note);
+
+        db.updateNote(n, Note.PICNIC_TABLE_NAME);
+
+        notesList.set(position, n);
+        mAdapter.notifyItemChanged(position);
+
+        toggleEmptyNotes();
+    }
+
+    private void deleteNote(int position) {
+        db.deleteNote(notesList.get(position), Note.PICNIC_TABLE_NAME);
+
+        notesList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+
+        toggleEmptyNotes();
+    }
+
+    private void showActionsDialog(final int position) {
+        CharSequence colors[] = new CharSequence[]{"Edit", "Delete"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Choose option");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    showNoteDialog(true, notesList.get(position), position);
+                } else {
+                    deleteNote(position);
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    private void showNoteDialog(final boolean shouldUpdate, final Note note, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.note_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(getContext());
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputNote = view.findViewById(R.id.note);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(!shouldUpdate ? "New Note" : "Edit Note");
+
+        if (shouldUpdate && note != null) {
+            inputNote.setText(note.getNote());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "update" : "save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+
+                    }
+                })
+                .setNegativeButton("cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(inputNote.getText().toString())) {
+                    Toast.makeText(getContext(), "Enter note!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+                if (shouldUpdate && note != null) {
+                    updateNote(inputNote.getText().toString(), position);
+                } else {
+                    createNote(inputNote.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void toggleEmptyNotes() {
+
+        if (db.getNotesCount(Note.PICNIC_TABLE_NAME) > 0) {
+            noNotesView.setVisibility(View.GONE);
+        } else {
+            noNotesView.setVisibility(View.VISIBLE);
+        }
     }
 }
